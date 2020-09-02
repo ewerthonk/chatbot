5c7b8fbfd93973cb35cc447d6e574f5f54d199eb
ewerthonk <70546174+ewerthonk@users.noreply.github.com>
2020-09-01 21:28:06 -0300

    Add files via upload

---
+++ Legendas/main.py
@@ -0,0 +1,28 @@
+import logging 
+from chatbot import MeuChatBot , filters
+from flask import Flask, render_template , request
+
+logger = logging.getLogger() 
+logger.setLevel(logging.CRITICAL)
+
+chatbot = MeuChatBot('Oscar', 
+                    filters=[filters.get_recent_repeated_responses]
+                    )
+
+chatbot.tratar_dados()
+chatbot.treina()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():    
+    return render_template("index2.html") 
+
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(chatbot.get_response(userText)) 
+
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="192.168.1.20", port=5002)
---
+++ chatbot.py
@@ -0,0 +1,35 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+import re
+
+class MeuChatBot(ChatBot):
+
+    def tratar_dados(self):
+        regex1 = r'\<.+\>\s|^\d+\n.+$\n.+\>|\-.+\-|\d+\:\d+\:\d+\,\d+\s\-\-\>\s\d+\:\d+\:\d+\,\d+|^\d+\s'
+        regex2 = r'^\s\s\s'
+        regex3 = r'^\s\-\s|^\-\s'
+        regex4 = r'^\s'
+        regex5 = r'$\n(?=[a-z]+)'
+        substituir1 = ''
+        substituir2 = ' '
+        diretorio = os.path.expanduser('~/Documents/Chatbot/Legendas/')
+        
+        for legenda in os.listdir(diretorio):
+            if legenda.endswith(".txt"):
+                abrir = open(diretorio+legenda, 'r')
+                fonte = abrir.read()
+                resultado = re.sub(regex1, substituir1, fonte, 0, re.MULTILINE)
+                resultado = re.sub(regex2, substituir1, resultado, 0, re.MULTILINE)
+                resultado = re.sub(regex3, substituir1, resultado, 0, re.MULTILINE)
+                resultado = re.sub(regex4, substituir1, resultado, 0, re.MULTILINE)
+                resultado = re.sub(regex5, substituir2, resultado, 0, re.MULTILINE)
+                abrir.close()
+                abrir = open('treino.txt', 'a')
+                abrir.write(resultado)
+
+    def treina(self):
+        self.trainer = ListTrainer(self)
+        abrir = open('treino.txt', 'r')
+        fonte = abrir.readlines()
+        self.trainer.train(fonte)
